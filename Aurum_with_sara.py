6927f6763e6c19a9ca4399cb78d9d59b7eb3a2d0
acarvalho-wcs <134070853+acarvalho-wcs@users.noreply.github.com>
2025-04-16 11:29:36 -0400

    Update Aurum_with_sara.py

--- Aurum_with_sara.py
+++ Aurum_with_sara.py
@@ -27,7 +27,7 @@ st.markdown("**Select an analysis from the sidebar to begin.**")
 # --- CONFIGURAÇÃO ---
 # Nome da aba do Google Sheets
 SHEET_NAME = "Aurum Gateway Data"
-SPREADSHEET_ID = "12xmK2MlbaY-5YDsJqvULKZ8r0tbNHjW8Xy2CgHsM-dw"
+SPREADSHEET_ID = "1HVYbot3Z9OBccBw7jKNw5acodwiQpfXgavDTIptSKic"
 
 import pygsheets
 
@@ -36,7 +36,7 @@ try:
     gc = pygsheets.authorize(service_account_info=st.secrets["gcp_service_account"])
 
     # Abre a planilha pelo ID
-    sheet_id = "12xmK2MlbaY-5YDsJqvULKZ8r0tbNHjW8Xy2CgHsM-dw"
+    sheet_id = "1HVYbot3Z9OBccBw7jKNw5acodwiQpfXgavDTIptSKic"
     spreadsheet = gc.open_by_key(sheet_id)
 
     st.success("✅ Conexão com Google Sheets realizada com sucesso!")
