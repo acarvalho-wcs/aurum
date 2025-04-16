b5b816a3175e5184acc876479c3d75c16a67f398
acarvalho-wcs <134070853+acarvalho-wcs@users.noreply.github.com>
2025-04-15 23:50:58 -0400

    Update Aurum_with_sara.py

--- Aurum_with_sara.py
+++ Aurum_with_sara.py
@@ -17,7 +17,7 @@ from sklearn.preprocessing import StandardScaler
 st.set_page_config(page_title="Aurum Dashboard", layout="wide")
 
 # Título e logotipo
-st.title("🐾 Aurum - Wildlife Crime Analysis Dashboard")
+st.title("**Aurum - Wildlife Crime Analysis Dashboard**")
 st.markdown("Select an analysis from the sidebar to begin.")
 
 # Upload do arquivo
