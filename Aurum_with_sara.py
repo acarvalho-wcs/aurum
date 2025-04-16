76c1107382ce394d8fbff8865f5c18a2e646514d
acarvalho-wcs <134070853+acarvalho-wcs@users.noreply.github.com>
2025-04-16 14:43:14 -0400

    Update Aurum_with_sara.py

--- Aurum_with_sara.py
+++ Aurum_with_sara.py
@@ -320,8 +320,6 @@ if uploaded_file is not None:
                     ax_c.plot(years, values, color='black', marker='o', label='Trend')
                     ax_c.plot(years, cusum_pos, linestyle='--', color='green', label='CUSUM+')
                     ax_c.plot(years, cusum_neg, linestyle='--', color='orange', label='CUSUM–')
-                    ax_c.axhline(0, color='gray', linewidth=0.6, linestyle='-', alpha=0.3)
-                    ax_c.axvline(years.min(), color='gray', linewidth=0.6, linestyle='-', alpha=0.3)
                     ax_c.grid(True, linestyle='--', linewidth=0.3, alpha=0.3)
 
                     ylabel = "Seized Specimens" if cusum_mode == "Total Seized per Year" else "N_seized (each seizure)"
