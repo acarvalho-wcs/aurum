8f37ddaa8b124970325d4f3033cad091b3d2beac
acarvalho-wcs <134070853+acarvalho-wcs@users.noreply.github.com>
2025-04-17 12:45:23 -0400

    Update Aurum_with_sara.py

--- Aurum_with_sara.py
+++ Aurum_with_sara.py
@@ -26,7 +26,6 @@ st.title("Aurum - Wildlife Trafficking Analytics")
 # Upload do arquivo
 st.sidebar.markdown("## Welcome to Aurum")
 st.sidebar.markdown("Log in below to unlock multi-user tools.")
-st.sidebar.markdown("**Start analysis here.**")
 st.sidebar.markdown("## 📂 Upload Data")
 uploaded_file = st.sidebar.file_uploader("**Upload your Excel file (.xlsx).**", type=["xlsx"])
 
