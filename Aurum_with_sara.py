3d4184647cb5e10b4faae98d9bcb4f1852a38fa7
acarvalho-wcs <134070853+acarvalho-wcs@users.noreply.github.com>
2025-04-21 17:14:38 -0400

    Update Aurum_with_sara.py

--- Aurum_with_sara.py
+++ Aurum_with_sara.py
@@ -24,6 +24,9 @@ st.set_page_config(page_title="Aurum Dashboard", layout="centered")
 st.title("Aurum - Wildlife Trafficking Analytics")
 
 # Upload do arquivo
+from PIL import Image
+logo = Image.open("logo.jpg")
+st.sidebar.image(logo, use_column_width=True)
 st.sidebar.markdown("## Welcome to Aurum")
 st.sidebar.markdown("Log in below to unlock multi-user tools.")
 show_about = st.sidebar.button("**About Aurum**")
