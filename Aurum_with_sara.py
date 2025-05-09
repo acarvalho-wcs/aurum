18ca0ee93e273f6b50bf71775cd76c52ee178992
Antônio F. Carvalho <134070853+acarvalho-wcs@users.noreply.github.com>
2025-05-09 19:10:37 -0400

    Update Aurum_with_sara.py

--- Aurum_with_sara.py
+++ Aurum_with_sara.py
@@ -1105,7 +1105,7 @@ def get_base64_image(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode()
 
-def place_logo_bottom_right(image_path, width=70, link_url="https://brasil.wcs.org/"):
+def place_logo_bottom_right(image_path, width=70, link_url="https://wcs.org/"):
     img_base64 = get_base64_image(image_path)
     st.markdown(
         f"""
